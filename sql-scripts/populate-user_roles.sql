55e5e4fe591d3b590f0ce2b80b8895c8167b26e2
Max Rademacher <maxrademacher@yahoo.com>
2015-05-31 15:52:39 -0400

    added script to populate user_roles

---
+++ sql-scripts/populate-user_roles.sql
@@ -0,0 +1,8 @@
+INSERT INTO user_roles (idAccount, idRole)
+VALUES (0, 0);
+
+INSERT INTO user_roles (idAccount, idRole)
+VALUES (1, 0);
+
+INSERT INTO user_roles (idAccount, idRole)
+VALUES (0, 1);
