dd8142715644f82f8806108454d9d28723ac5e78
Max Rademacher <maxrademacher@yahoo.com>
2015-06-09 19:27:33 -0400

    cleaned up database package

--- src/database/Database.java
+++ src/database/Database.java
@@ -120,4 +120,21 @@ public class Database implements IDatabase {
 	public void close() throws Exception {
 		connection.close();
 	}
+	
+	
+	/**
+	 * Processes rows from the database one row at a time
+	 */
+	private interface RowProcessor<R, T extends Exception> {
+	
+		/**
+		 * Processes a row from the database
+		 * 
+		 * @param row the row to process
+		 * @param t the result from processing the last row
+		 * @return the result from processing this row
+		 */
+		public R process(Row row, R t) throws T;
+	}
+	
 }
