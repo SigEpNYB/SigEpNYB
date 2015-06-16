97608ece6f6fcdbb1f5347a8e6470b82a81e1f40
Max Rademacher <maxrademacher@yahoo.com>
2015-06-15 21:40:44 -0400

    added a user with absolutely no roles or permissions to the database for testing

--- sql-scripts/populate-accounts.sql
+++ sql-scripts/populate-accounts.sql
@@ -3,3 +3,6 @@ VALUES ('mtr73', 'pass1', 'Max', 'Rademacher');
 
 INSERT INTO accounts (netid, password, firstName, lastName)
 VALUES ('net123', 'letmein', 'Joe', 'Shmoe');
+
+INSERT INTO accounts (netid, password, firstName, lastName)
+VALUE ('nofun', 'plz', 'No', 'Permissions');
