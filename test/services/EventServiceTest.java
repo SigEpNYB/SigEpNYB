bf23f5b079ca959bad03d95aad1e0611b3b73d2f
Max Rademacher <maxrademacher@yahoo.com>
2015-06-16 21:38:53 -0400

    added some event service testing

---
+++ test/services/EventServiceTest.java
@@ -0,0 +1,51 @@
+package services;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import data.Event;
+import exceptions.InternalServerException;
+import exceptions.InvalidCredentialsException;
+import exceptions.InvalidTokenException;
+import exceptions.PermissionDeniedException;
+
+public class EventServiceTest {
+	EventService service;
+	
+	@Before
+	public void setUp() throws Exception {
+		service = Services.getEventService();
+	}
+
+	@Test
+	public void testCreate() throws InternalServerException, PermissionDeniedException, InvalidTokenException, InvalidCredentialsException {
+		TokenService tokenService = Services.getTokenService();
+		String token = tokenService.login("mtr73", "pass1");
+		
+		Date now = Calendar.getInstance().getTime();
+		service.create(token, "bla", now, now, "blablabla");
+		Event[] events = service.get(token, now, now);
+		
+		Event event = null;
+		for (Event e : events) {
+			if (e.getTitle().equals("bla")) {
+				event = e;
+				break;
+			}
+		}
+		
+		assertNotNull(event);
+		
+		service.cancel(token, event.getId());
+		
+		tokenService.logout(token);
+	}
+
+	//TODO write more tests
+
+}
