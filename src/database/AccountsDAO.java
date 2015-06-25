03b655f47115030cb5cf2c6790afbbad62fb6779
Max Rademacher <maxrademacher@yahoo.com>
2015-06-24 20:47:30 -0400

    integrated new row functionality with AccountsDAO

--- src/database/AccountsDAO.java
+++ src/database/AccountsDAO.java
@@ -17,9 +17,6 @@ public class AccountsDAO {
 	public static final int ACCOUNT_NOT_FOUND = -1;
 	
 	private static final String IDACCOUNT = "idAccount";
-	private static final String NETID = "netid";
-	private static final String FIRSTNAME = "firstName";
-	private static final String LASTNAME = "lastName";
 	
 	private static final String CREATE_ACCOUNT_SQL = "INSERT INTO accounts (netid, password, firstName, lastName) VALUES ('%s', '%s', '%s', '%s')";
 	private static final String GET_IDACCOUNT_NET_PASS_SQL = "SELECT idAccount FROM accounts WHERE netid = '%s' AND password = '%s'";
@@ -50,24 +47,15 @@ public class AccountsDAO {
 		return database.execute((row, t) -> row.getInt(IDACCOUNT), ACCOUNT_NOT_FOUND, GET_IDACCOUNT_NET_SQL, netid);
 	}
 	
-	/** Builds an AccountData from a given row */
-	private AccountData build(Row row) throws SQLException {
-		int idAccount = row.getInt(IDACCOUNT);
-		String netid = row.getString(NETID);
-		String firstName = row.getString(FIRSTNAME);
-		String lastName = row.getString(LASTNAME);
-		return new AccountData(idAccount, netid, firstName, lastName);
-	}
-	
 	/** Gets the account for the given token */
 	public AccountData get(int idAccount) throws SQLException {
-		return database.execute((row, t) -> build(row), null, GET_ACCOUNT_SQL, idAccount);
+		return database.execute((row, t) -> row.build(AccountData.class), null, GET_ACCOUNT_SQL, idAccount);
 	}
 	
 	/** Gets a list of all the accounts */
 	public AccountData[] getAccounts() throws SQLException {
 		List<AccountData> accounts = database.execute(
-				(row, lst) -> {lst.add(build(row)); return lst;}, 
+				(row, lst) -> {lst.add(row.build(AccountData.class)); return lst;}, 
 				new LinkedList<AccountData>(), GET_ACCOUNTS_SQL);
 		return accounts.toArray(new AccountData[accounts.size()]);
 	}
