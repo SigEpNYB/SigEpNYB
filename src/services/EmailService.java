40b05170da7970f561bfe6076173345dba874a58
Max Rademacher <maxrademacher@yahoo.com>
2015-07-23 15:17:21 -0400

    SLG3: moved the actual sending of the email to a separate, internal thread to speed up requests

--- src/services/EmailService.java
+++ src/services/EmailService.java
@@ -6,6 +6,8 @@ package services;
 import java.io.File;
 import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -20,19 +22,39 @@ import javax.mail.internet.MimeMessage;
  * Sends emails
  */
 class EmailService {
+	private static final int QUEUE_CAPACITY = 100;
+	
 	private final String userName;
 	private final String password;
+	
+	private final BlockingQueue<Email> emailQueue;
 	
 	/** Creates a new EmailService */
 	public EmailService(String userName, String password) {
 		this.userName = userName;
 		this.password = password;
+		emailQueue = new ArrayBlockingQueue<>(QUEUE_CAPACITY);
+		
+		Thread thread = new Thread(() -> run());
+		thread.start();
 	}
 	
-	/** Sends an email to the given person with the subject and message 
-	 * @throws MessagingException 
-	 * @throws AddressException */
-	void send(String toNetid, String subject, String content) throws AddressException, MessagingException {
+	/** Internal method the continuously processes and sends emails */
+	private void run() {
+		while (true) {
+			try {
+				Email email = emailQueue.take();
+				process(email);
+				
+				Thread.sleep(10);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	/** Sends the given email */
+	private void process(Email email) throws AddressException, MessagingException {
 		Properties props = new Properties();
 		props.put("mail.smtp.host", "smtp.gmail.com");
 		props.put("mail.smtp.socketFactory.port", "465");
@@ -51,13 +73,18 @@ class EmailService {
 		Message message = new MimeMessage(session);
 		message.setFrom(new InternetAddress(userName + "@gmail.com"));
 		message.setRecipients(Message.RecipientType.TO,
-				InternetAddress.parse(toNetid + "@cornell.edu"));
-		if (subject != null) {
-			message.setSubject(subject);
+				InternetAddress.parse(email.to));
+		if (email.sbj != null) {
+			message.setSubject(email.sbj);
 		}
-		message.setText(content);
+		message.setText(email.msg);
 
 		Transport.send(message);
+	}
+	
+	/** Sends an email to the given person with the subject and message */
+	void send(String toNetid, String subject, String content) throws InterruptedException {
+		emailQueue.put(new Email(toNetid + "@cornell.edu", subject, content));
 	}
 	
 	/** Sends an email to the given person with the subject and body file */
@@ -68,6 +95,20 @@ class EmailService {
 	/** Sends an email to the given person with the subject, template file, and parameters */
 	void send(String toNetid, String subject, File templateFile, Map<String, Object> params) {
 		
+	}
+	
+	/** Stores information about an email */
+	private class Email {
+		private final String to;
+		private final String sbj;
+		private final String msg;
+		
+		/** Creates a new Email */
+		Email(String to, String sbj, String msg) {
+			this.to = to;
+			this.sbj = sbj;
+			this.msg = msg;
+		}
 	}
 	
 }
