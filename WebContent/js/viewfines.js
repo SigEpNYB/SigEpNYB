6bca9023ba962d479b2c83ccf14a498a2c534ab7
DecSander <decy23@gmail.com>
2016-02-11 16:54:32 -0500

    prep for merge

--- WebContent/js/viewfines.js
+++ WebContent/js/viewfines.js
@@ -5,7 +5,7 @@ sendRequest('GET', 'Fines', null, 'json', true, null, function(fines) {
     '<td>$' + fine.amount + '</td>' +
     '<td><a href="https://venmo.com/" class="btn btn-primary">Pay it on Venmo</a></td>' +
     '</tr>';
-  });
+  }).reduce(addStr, '');
   document.getElementById('fines').innerHTML = finesString;
 }, function(xhr) {
   swal({
