76dac745d2b824168d74a3bc03171f215e2f0c7f
Max Rademacher <maxrademacher@yahoo.com>
2016-12-17 17:14:48 -0500

    Updated max's initial password

--- sql-scripts/populate-accounts.sql
+++ sql-scripts/populate-accounts.sql
@@ -1,2 +1,2 @@
 INSERT INTO accounts (netid, password, firstName, lastName, phone)
-VALUES ('mtr73', '$2a$10$hy0pe7daephkpke6bRrgeuXqCTzBGE04D/ZldMdwf/v9Zxv3XzOl6', 'Max', 'Rademacher', '(401) 440 - 4218');
+VALUES ('mtr73', '$2a$10$FokYLZMZHv/B0aOJlymfheBkx8lmMKbACdcPB8EFuImmgpMxHlPCq', 'Max', 'Rademacher', '(401) 440 - 4218');
